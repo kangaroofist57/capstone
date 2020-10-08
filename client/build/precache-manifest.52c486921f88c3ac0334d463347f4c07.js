8c8025033ed95c8601bd401079b93b39cabd09a0
kangaroofist57 <solanodaniel57@yahoo.com>
2020-10-08 03:59:28 -0400

    testing heroku removed scripts and added build folder

---
+++ client/build/precache-manifest.52c486921f88c3ac0334d463347f4c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee2af26dda0469ceaacb6ef6940e33f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b920be8e472cb449148",
+    "url": "/static/css/main.92900016.chunk.css"
+  },
+  {
+    "revision": "6c4d31eaedb3a793c3de",
+    "url": "/static/js/2.1ba0c8a7.chunk.js"
+  },
+  {
+    "revision": "98c92e4a71422a59c3daed74f16a0206",
+    "url": "/static/js/2.1ba0c8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b920be8e472cb449148",
+    "url": "/static/js/main.aa7d3f9e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
